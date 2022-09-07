7e6ef9ec3fee25fbdce33df4dd21d3158151a168
Lennard Ekrod <lennard.ekrod@student.hpi.uni-potsdam.de>
2022-09-07 17:34:42 +0200

    code for plot generation for sim + kiel data done

--- src/visualisation/visualize.py
+++ src/visualisation/visualize.py
@@ -1,5 +1,6 @@
 from cmath import nan
 from enum import auto
+from turtle import title
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -10,22 +11,24 @@ matplotlib.use("WebAgg")
 
 df_sim_right = pd.read_csv("data_sim/aggregated_data/all_right_parameters.csv")
 df_sim_left = pd.read_csv("data_sim/aggregated_data/all_left_parameters.csv")
+data_kiel_right = pd.read_csv("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/data_kiel/aggregated_data/all_right_parameters.csv")
 
 def df_check_outlier_column(df):
     print(df.shape)
     print(df.info()) #last two columns are dtype object
     print("old_describe")
     print(df.describe())
-    # df_sim_right['method'] = df_sim_right['method'].astype('category')
-    # df_sim_right['subject'] = df_sim_right['subject'].astype('category')
+    #df['method'] = df['method'].astype('category')
+    #df['subject'] = df['subject'].astype('category')
     print('old shape_' , df.shape)
 
     #''' Removing the Outliers '''
     print("outlier = ", sum(df["is_outlier"] == True))
-    df_is_outlier = df[df["is_outlier"] == False]
-    print(df_is_outlier)
-    sns.boxplot( x= 'subject', y='stride_length',data=df_is_outlier)
+    df  = df[df["is_outlier"] == False]
+    print(df)
+    sns.boxplot( x= 'subject', y='stride_length',data=df)
     plt.show()
+    return df
 
 #IQR Method
 def iqr_outliers(df, feature):
@@ -52,6 +55,7 @@ def exclude_outlier(df):
 
     # check for selected outlier set to NA
     print("describe: ", df.describe())
+    print("subject unique before drop", df.subject.unique())
     print("sum of NAs: ", df.isnull().sum())
 
     # drop selected outlier = NA
@@ -62,48 +66,86 @@ def exclude_outlier(df):
 
     print(df.describe())
     print(df.method.unique())
+    print(df.subject.unique())
     print(df.method.value_counts())
+    print(df.subject.value_counts())
     
     return df
 
 
-def plot_check_df(df, name):
+def plot_check_df(df, name, color_dict):
     df_corr = df.iloc[:,:-6]
     print("df_corr_", df_corr.columns.tolist())
+    plt.figure(figsize = (15,8))
     sns.heatmap(df_corr.corr(), xticklabels = 1, yticklabels = 1)
     plt.show()
     plt.close()
     sns.boxplot(x='variable', y='value', data = pd.melt(df_corr.loc[:,["stride_index", 'timestamp','fo_time', 'ic_time']]))
-    plt.show()
+    #plt.show()
     plt.close()
     sns.boxplot(x='variable', y='value', data = pd.melt(df_corr.loc[:,['stride_length', 'clearance', 'stride_time',
     'swing_time', 'stance_time', 'stance_ratio']]))
-    plt.show()
+    #plt.show()
     plt.close()
-    sns.boxplot(x= 'subject', y='stride_length',data=df)
-    plt.show()
+    sns.boxplot(x= 'subject', y='stride_length',data=df).set(title= "stride length of the different stroke categories " + name)
+    #plt.show()
     plt.close()
-    sns.boxplot(x= 'method', y='stride_length',data=df)
-    plt.show()
+    sns.boxplot(x= 'method', y='stride_length', data=df, palette= color_dict, order = color_dict).set(title= "stride length of the different stroke categories " + name)
+    #plt.show()
     plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ name +"_stride_length_.png")  
     plt.close()
     sns.boxplot(x= "subject", y='clearance',data=df)
-    plt.show()
+    #plt.show()
     plt.close()
-    sns.boxplot(x= "method", y='clearance',data=df)
-    plt.show()
+    sns.boxplot(x= "method", y='clearance',data=df, palette= color_dict, order = color_dict).set(title= "clearance of the different stroke categories " + name)
+    #plt.show()
     plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ name +"_clearance_.png") 
     plt.close()
     sns.boxplot( x= "subject", y='stride_time',data=df)
-    plt.show()
+    #plt.show()
     plt.close()
-    sns.boxplot( x= "method", y='stride_time',data=df)
-    plt.show()
+    sns.boxplot( x= "method", y='stride_time',data=df, palette= color_dict, order = color_dict).set(title= "stride time of the different stroke categories " + name)
+    #plt.show()
     plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ name +"_stride_time_.png") 
     plt.close()
     sns.pairplot(df_corr)
-    plt.show()
+    #plt.show()
 
+
+def subject_boxplots(df, name, subject, color_dict):
+    sns.boxplot(x= df.loc[df["subject"] == subject, 'method'], y='stride_length', data=df, palette= color_dict, order = color_dict).set(title= "stride length of the different stroke categories " + name)
+    plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ subject + name +"_stride_length_.png") 
+    plt.close() 
+    
+    sns.boxplot(x= df.loc[df["subject"] == subject, 'method'], y='clearance',data=df, palette= color_dict, order = color_dict).set(title= "clearance of the different stroke categories " + name)
+    plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ subject + name +"_clearance_.png") 
+    plt.close()
+    
+    sns.boxplot( x= df.loc[df["subject"] == subject, 'method'], y='stride_time',data=df, palette= color_dict, order = color_dict).set(title= "stride time of the different stroke categories " + name)
+    plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ subject + name +"_stride_time_.png") 
+    plt.close()
+
+def plot_check_kiel(df, name):
+    df_corr = df.iloc[:,:-6]
+    print("df_corr_", df_corr.columns.tolist())
+    plt.figure(figsize = (15,8))
+    sns.heatmap(df_corr.corr(), xticklabels = 1, yticklabels = 1)
+    plt.show()
+    plt.close()
+    sns.boxplot(x='variable', y='value', data = pd.melt(df_corr.loc[:,["stride_index", 'timestamp','fo_time', 'ic_time']]))
+    plt.close()
+    sns.boxplot(x='variable', y='value', data = pd.melt(df_corr.loc[:,['stride_length', 'clearance', 'stride_time',
+    'swing_time', 'stance_time', 'stance_ratio']]))
+    plt.close()
+    sns.boxplot(x= 'subject', y='stride_length',data = df).set(title = "stride length of the different stroke categories " + name)
+    plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ name +"_stride_length_.png")  
+    plt.close()
+    sns.boxplot(x= "subject", y='clearance',data = df).set(title = "clearance of the different stroke categories " + name)
+    plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ name +"_clearance_.png") 
+    plt.close()
+    sns.boxplot( x= "subject", y='stride_time',data = df).set(title = "stride time of the different stroke categories " + name)
+    plt.savefig("/dhc/home/lennard.ekrod/Masterthesis_Lennard_E/figures/"+ name +"_stride_time_.png") 
+    plt.close()
 # fig, ax = plt.subplots(figsize = (18,10))
 # ax.scatter(df_sim_right['method'], df_sim_right['stance_time'])
 
@@ -115,8 +157,18 @@ def plot_check_df(df, name):
 # plt.show()
 
 ########## MAIN #########
-df_sim_right = exclude_outlier(df_sim_right)
-df_sim_left = exclude_outlier(df_sim_left)
-#print(df_sim_right.describe())
-plot_check_df(df_sim_right, "sim_right")
-plot_check_df(df_sim_left, "sim_left")
+## Data Sim
+# df_sim_right = exclude_outlier(df_sim_right)
+# df_sim_left = exclude_outlier(df_sim_left)
+# print(df_sim_right.describe())
+# color_dict_sim = {"regular":"C0", "1P":"C1", "2P":"C2", "3P":"C3"}
+#plot_check_df(df_sim_right, "sim_right", color_dict_sim)
+#plot_check_df(df_sim_left, "sim_left", color_dict_sim)
+#subject_boxplots(df_sim_left, "_sim_left", "S1", color_dict_sim)
+#subject_boxplots(df_sim_left, "_sim_left", "S2", color_dict_sim)
+
+## Data Kiel
+data_kiel_right = df_check_outlier_column(data_kiel_right)
+# df_kiel_right = exclude_outlier(data_kiel_right)
+# plot_check_kiel(df_kiel_right, "kiel_right")
+plot_check_kiel(data_kiel_right, "kiel_right")
